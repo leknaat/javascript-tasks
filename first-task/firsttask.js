b5e93e29bbb06bb2b7b61c8a5c4a1751217b8d37
leknaat <wishfire@gmail.com>
2017-02-02 10:23:17 +0000

    Added comments for clarity

--- first-task/firsttask.js
+++ first-task/firsttask.js
@@ -1,7 +1,16 @@
+// setting up variables
 var firstName = "Richard";
 var lastName = "Gill";
+
+
+
+// printing to console
 console.log(firstName + " " + lastName);
 
+
+
+// setting up functions to print to the console
+// i could have made them variables but i wasn't sure if i 'should'
 function firstNameLastName(firstName, lastName) {
 	console.log(firstName + " " + lastName);
 };
@@ -15,8 +24,16 @@ function lastNameFirstName(lastName, firstName) {
 lastNameFirstName(lastName, firstName);
 
 
+
+// finding the length of the var when the string is "Richard"
 var firstName = "Richard";
 firstName.length;
 
+
+
+// creating the array of people and then printing to console
 var people = ['Jack', 'John', 'James'];
 console.log(people);
+
+
+// function which take a name and returns it without the first character
