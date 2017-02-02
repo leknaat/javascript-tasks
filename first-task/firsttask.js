d837dc9d8d4989f7a022fb55a949e4948fd0c13c
leknaat <wishfire@gmail.com>
2017-02-02 10:15:08 +0000

    All the easy tasks done! Time to get working on the last three.

---
+++ first-task/firsttask.js
@@ -0,0 +1,22 @@
+var firstName = "Richard";
+var lastName = "Gill";
+console.log(firstName + " " + lastName);
+
+function firstNameLastName(firstName, lastName) {
+	console.log(firstName + " " + lastName);
+};
+
+firstNameLastName(firstName, lastName);
+
+function lastNameFirstName(lastName, firstName) {
+	console.log(lastName + ", " + firstName);
+};
+
+lastNameFirstName(lastName, firstName);
+
+
+var firstName = "Richard";
+firstName.length;
+
+var people = ['Jack', 'John', 'James'];
+console.log(people);
